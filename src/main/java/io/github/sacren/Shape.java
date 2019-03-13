0c61f3b759996347339350f82d4e3e77bb600a60
Jεan Sacren <sakiwit@gmail.com>
2019-03-13 04:56:27 -0600

    Add Shape interface for Rectangle and Triangle

---
+++ src/main/java/io/github/sacren/Shape.java
@@ -0,0 +1,13 @@
+/**
+ * Shape interface for Rectangle and Triangle subclasses.
+ *
+ * <p>Public interface:
+ * +getArea():double
+ */
+public interface Shape { /* keyword "interface", not "class" */
+    /* "public" and "abstract" are assumed.
+     * "protected", "private" and "package" are not allowed.
+     * Method signature will be implemented in subclasses.
+     */
+    double getArea();
+}
