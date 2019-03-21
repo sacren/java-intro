487fa81abb8b0966ded4a6540ddad043796a26b3
Jεan Sacren <sakiwit@gmail.com>
2019-03-21 01:45:00 -0600

    Use character validation without switch statement

--- src/main/java/io/github/sacren/Hex.java
+++ src/main/java/io/github/sacren/Hex.java
@@ -36,34 +36,10 @@ public class Hex {
             return -1;
         }
         for (char c : a) {
-            switch (c) {
-                case '0':
-                case '1':
-                case '2':
-                case '3':
-                case '4':
-                case '5':
-                case '6':
-                case '7':
-                case '8':
-                case '9':
-                case 'a':
-                case 'A':
-                case 'b':
-                case 'B':
-                case 'c':
-                case 'C':
-                case 'd':
-                case 'D':
-                case 'e':
-                case 'E':
-                case 'f':
-                case 'F':
-                    decimal += Math.pow(16, a.length - ++index) * Character.digit(c, 16);
-                    break;
-                default:
-                    return -1;
+            if (Character.digit(c, 16) == -1) {
+                return -1;
             }
+            decimal += Math.pow(16, a.length - ++index) * Character.digit(c, 16);
         }
         return decimal;
     }
