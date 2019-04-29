e7ea81d5918f3419926e48242515b41694021922
Jεan Sacren <sakiwit@gmail.com>
2019-04-29 01:38:30 -0600

    Add Hex method for user input validation

--- src/main/java/io/github/sacren/Hex.java
+++ src/main/java/io/github/sacren/Hex.java
@@ -9,10 +9,14 @@
  * +Hex(userStr:String)
  *
  * <p>Public methods:
+ * +check():void
  * +decimal():void
  * +toString():String
  */
 public class Hex {
+    /* private static data */
+    private static final int HEX = 16;
+
     /* private instance data */
     private String userStr;
     private long decimal;
@@ -20,41 +24,40 @@ public class Hex {
     /** Hex constructor. */
     public Hex(String userStr) {
         this.userStr = userStr;
+        check();
         decimal();
+    }
+
+    /** Hex method to validate user input. */
+    public void check() {
+        final char[] a = userStr.toCharArray();
+        /* throw if the user doesn't have input */
+        if (a.length == 0) {
+            throw new IllegalArgumentException(
+                    String.format("\"%s\" is invalid!", userStr));
+        }
+        for (char c : a) {
+            if (Character.digit(c, HEX) == -1) {
+                throw new IllegalArgumentException(
+                        String.format("\"%s\" is invalid!", userStr));
+            }
+        }
     }
 
     /** Hex method of conversion to the decimal number. */
     public void decimal() {
         final char[] a = userStr.toCharArray();
-        final int hex = 16;
         int index = 0;
-        int digit;
-        /* throw if the user doesn't have input */
-        if (a.length == 0) {
-            decimal = -1;
-            return;
-        }
-        /* convert to decimal */
-        decimal = 0;
         for (char c : a) {
-            digit = Character.digit(c, hex);
-            if (digit == -1) {
-                decimal = -1;
-                return;
-            }
             /* (a.length - 1) is the position of the most significant hex bit.
              * (a.length - ++index) happens to be the exponent.
              */
-            decimal += Math.pow(hex, a.length - ++index) * digit;
+            decimal += Math.pow(HEX, a.length - ++index) * Character.digit(c, HEX);
         }
     }
 
     /** Hex instance description. */
     public String toString() {
-        if (decimal == -1) {
-            throw new IllegalArgumentException(
-                    String.format("\"%s\" is invalid!", userStr));
-        }
         return String.format(
                 "Hex:     %s%n"
                         + "Decimal: %d",
