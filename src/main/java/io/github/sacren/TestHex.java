84d694af982a82ae8a21f643c91adeaf3d0d073f
Jεan Sacren <sakiwit@gmail.com>
2019-04-30 01:03:30 -0600

    Update test driver for Hex class

--- src/main/java/io/github/sacren/TestHex.java
+++ src/main/java/io/github/sacren/TestHex.java
@@ -7,13 +7,13 @@ public class TestHex {
     /** Hex test driver. */
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        String usrStr;
         System.out.print("Enter a hex number: ");
-        String hexStr = in.nextLine();
+        usrStr = in.nextLine();
         in.close();
-        Hex hex = new Hex(hexStr);
         System.out.println();
-        System.out.println("=== Hex conversion to decimal ===");
+        System.out.println("=== Hex to decimal ===");
         System.out.println();
-        System.out.println(hex);
+        System.out.println(new Hex(usrStr));
     }
 }
